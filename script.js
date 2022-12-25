53857ad5c42b4d2fcce89956fef72a0ca2b25739
Lê Văn Bảo Thọ <93852662+levanbaotho2752007@users.noreply.github.com>
2022-12-26 01:24:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//fix code and by btho source gốc không nhớ :(((
 const toggle = document.querySelector(".toggle__theme");
 const card = document.querySelector(".app");
 toggle.addEventListener("click", () => {
@@ -15,7 +16,6 @@ toggle.addEventListener("click", () => {
     }
 })
 
-//Chống copy
 function killCopy(e){
     return false;
 }
@@ -39,30 +39,23 @@ function noteOut()
 
 setInterval(noteOut,3000);
 
-//Chống chuột phải 
 window.onload = function() {
     document.addEventListener("contextmenu", function(e) {
         e.preventDefault();
     }, false);
     document.addEventListener("keydown", function(e) {
-        //document.onkeydown = function(e) {
-        // "I" key
         if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
             disabledEvent(e);
         }
-        // "J" key
         if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
             disabledEvent(e);
         }
-        // "S" key + macOS
         if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
             disabledEvent(e);
         }
-        // "U" key
         if (e.ctrlKey && e.keyCode == 85) {
             disabledEvent(e);
         }
-        // "F12" key
         if (event.keyCode == 123) {
             disabledEvent(e);
         }
@@ -78,8 +71,6 @@ window.onload = function() {
         return false;
     }
 };
-
-//Chống Ctrl + U
 document.onkeydown = function(e) {
     if (e.ctrlKey && 
         (e.keyCode === 67 || 
