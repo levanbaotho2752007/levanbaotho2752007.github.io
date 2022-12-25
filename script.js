12cc99bec51c99762fce5383837549f684d7f03f
Lê Văn Bảo Thọ <93852662+levanbaotho2752007@users.noreply.github.com>
2022-12-25 23:24:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const card = document.querySelector(".app");
 toggle.addEventListener("click", () => {
     let theme = toggle.querySelector(".fas");
     if (theme.classList.contains("fa-moon")) {
-        theme.classList.remove("fa-moon");
-        theme.classList.add("fa-sun");
-        card.classList.remove ("dark");
-    } else {
         theme.classList.remove("fa-sun");
         theme.classList.add("fa-moon");
         card.classList.add("dark");
+    } else {
+        theme.classList.remove("fa-moon");
+        theme.classList.add("fa-sun");
+        card.classList.remove ("dark");
     }
 })
 
