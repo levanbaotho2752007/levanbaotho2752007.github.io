23d80711c0eb93781b67042ed73ccd343db5afed
Lê Văn Bảo Thọ <93852662+levanbaotho2752007@users.noreply.github.com>
2022-12-25 22:57:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const toggle = document.querySelector(".toggle__theme");
 const card = document.querySelector(".app");
 let theme = toggle.querySelector(".fas")
-theme.classList.remove("fa-sun");
-theme.classList.add("fa-moon");
+theme.classList.add("fa-sun");
+theme.classList.remove ("fa-moon");
 card.classList.add("dark");
 // toggle.addEventListener("click", () => {
 //     let theme = toggle.querySelector(".fas");
